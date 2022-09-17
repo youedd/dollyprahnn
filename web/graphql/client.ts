4c9df3f60a345a53d57f77995080410c652b881b
youedd <eddibili.youssef@gmail.com>
2022-09-17 16:47:40 +0200

    migrate from  next to astor

--- web/graphql/client.ts
+++ web/graphql/client.ts
@@ -2,7 +2,7 @@ import { GraphQLClient } from "graphql-request"
 import { getSdk } from "./generated/sdk"
 
 const client = () => {
-  const endpoint = process.env.GRAPHQL_ENDPOINT;
+  const endpoint = import.meta.env.GRAPHQL_ENDPOINT;
 
   if (!endpoint) {
     throw "GRAPHQL_ENDPOINT env variable is not defiend"
